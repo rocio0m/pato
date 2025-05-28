c564635f04fc04befab3d62ee30fef72622f4a9f
rocio0m <dammuturunco123@gmail.com>
2025-05-28 12:00:59 +0000

    28 de mayo

--- rocio2.py
+++ rocio2.py
@@ -9,3 +9,14 @@ while noAdivinado:
     numeroIngresado= int (input("adivina el numero (entre 1 y 100):" ))
     if rangoCorrecto(numeroIngresado):
         print("por")
+
+ MIERCOLES 28 DE MAYO 
+
+    def nombreDeFuncion(parametros, argumentos):
+        print("esto es una funcion")
+nombreDeFuncion("parametros",
+                "esto es unn argumento")
+def ingresarNumero(mensaje):
+    entrada = input(mensaje)
+    esEntero = int(entrada)
+    return esEntero
